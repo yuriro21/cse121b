999306de702d4f9d06c44db8a778de1686c84235
yuriro21 <edisson.ruiz21@gmail.com>
2022-10-09 21:43:38 -0600

    week 4 task done

--- week04/scripts/gpa.js
+++ week04/scripts/gpa.js
@@ -1,28 +1,55 @@
 function getGrades(inputSelector) {
     // get grades from the input box
+    const grades = document.querySelector("#grades").value;
     // split them into an array (String.split(','))
+    const gradesArray = grades.split(",");
     // clean up any extra spaces, and make the grades all uppercase. (Array.map())
+    const cleanGrades = gradesArray.map((grade) => grade.trim().toUpperCase());
+    console.log(cleanGrades);
     // return grades
-}
-
-function lookupGrade(grade) {
+    return cleanGrades;
+  }
+  
+  function lookupGrade(grade) {
     // converts the letter grade to it's GPA point value and returns it
-}
-
-function calculateGpa(grades) {
+    let points = 0;
+    if (grade === "A") {
+      points = 4;
+    } else if (grade === "B") {
+      points = 3;
+    } else if (grade === "C") {
+      points = 2;
+    } else if (grade === "D") {
+      points = 1;
+    }
+    return points;
+  }
+  
+  function calculateGpa(grades) {
     // gets a list of grades passed in
     // convert the letter grades to gpa points
+    const gradePoints = grades.map((grade) => lookupGrade(grade));
     // calculates the GPA
+    const gpa =
+      gradePoints.reduce((total, num) => total + num) / gradePoints.length;
     // return the GPA
-}
-
-function outputGpa(gpa, selector) {
-    // takes a gpa value and displays it in the HTML in the element identified by the selector
-}
-
-function clickHandler() {
-    // when the button in our html is clicked:
+    return gpa.toFixed(2);
+  }
+  
+  function outputGpa(gpa, selector) {
+    // takes a gpa value and displays it in the HTML in the element identified by the selector passed in
+    const outputElement = document.querySelector(selector);
+    outputElement.innerText = gpa;
+  }
+  
+  function clickHandler() {
+    // when the button in our html is clicked
     // get the grades entered into the input
+    const grades = getGrades();
     // calculate the gpa from the grades entered
+    const gpa = calculateGpa(grades);
     // display the gpa
-}
+    outputGpa(gpa, "#output");
+  }
+  
+  document.querySelector("#submitButton").addEventListener("click", clickHandler);
